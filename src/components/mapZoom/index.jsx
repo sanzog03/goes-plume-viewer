68492db0b75a7ebca3f4568237057619bd8d84f3
Sanjog Thapa <sanzog03@gmail.com>
2024-11-07 15:12:22 -0600

    center the selected plume.

--- src/components/mapZoom/index.jsx
+++ src/components/mapZoom/index.jsx
@@ -10,11 +10,12 @@ export const MapZoom = ({ zoomLevel }) => {
 
         const [lon, lat] = zoomLevel;
         map.flyTo({
-            center: [lon-0.001, lat], // Replace with the desired latitude and longitude
-            zoom: 7,
+            center: [lon, lat], // Replace with the desired latitude and longitude
+            offset: [-250, 0],
+            zoom: 7.5,
         });
 
-    }, [zoomLevel]);
+    }, [map, zoomLevel]);
 
     return null;
 }
